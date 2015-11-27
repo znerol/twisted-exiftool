18c60b544e47534e1655181d09edd4c2077772c2
Lorenz Schori <lo@znerol.ch>
2015-11-27 16:52:06 +0100

    Add classifiers

--- setup.py
+++ setup.py
@@ -17,5 +17,19 @@ setup(
         'Twisted>=13.1',
         'zope.interface'
     ],
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Multimedia :: Graphics :: Capture'
+    ],
 )
