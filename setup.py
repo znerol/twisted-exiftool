a38971a4994162acf0a530d3139393bcbdb0d467
Lorenz Schori <lo@znerol.ch>
2015-11-27 14:59:06 +0100

    Prepare for stable release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='TwistedExiftool',
-    version='0.0.1',
+    version='0.1.0',
     description='Exiftool protocol and stream endpoint plugin to be used with twisted',
     author='Lorenz Schori',
     author_email='lo@znerol.ch',
