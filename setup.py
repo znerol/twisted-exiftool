1f8d608e07b754ac755635775517371ebf5c17a2
Lorenz Schori <lo@znerol.ch>
2015-11-27 16:43:00 +0100

    Add repository url

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='Exiftool protocol and stream endpoint plugin to be used with twisted',
     author='Lorenz Schori',
     author_email='lo@znerol.ch',
+    url='https://github.com/znerol/twisted-exiftool',
     packages=['twisted_exiftool'],
     package_data={
         'twisted.plugins': [
