812fc08140db65644ff666d03ae455092bad7b43
Lorenz Schori <lo@znerol.ch>
2016-01-17 00:04:00 +0100

    Final release, point to new project

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='TwistedExiftool',
-    version='0.1.3',
-    description='Exiftool protocol and stream endpoint plugin to be used with twisted',
+    version='0.1.4',
+    description='Discontinued, use txExiftool (https://pypi.python.org/pypi/txExiftool)',
     author='Lorenz Schori',
     author_email='lo@znerol.ch',
     url='https://github.com/znerol/twisted-exiftool',
